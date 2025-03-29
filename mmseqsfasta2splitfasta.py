c3746e55e87dc120f915de69a62622ee0cc0b87e
jlingford <james.lingford@gmail.com>
2025-03-30 09:30:38 +1100

    fix: mmseqs fasta splitting python script works as intended now

--- mmseqsfasta2splitfasta.py
+++ mmseqsfasta2splitfasta.py
@@ -5,6 +5,7 @@ The mmseqs fasta outputs denote different clusters with identical fasta headers 
 This script scans the fasta file for those identical and consecutive lines, and splits them into separate files.
 """
 
+import re
 import argparse
 
 # accept one positional argument (the input file)
@@ -21,28 +22,50 @@ args = parser.parse_args()
 
 
 # initialize variables
-file_count = 0
-current_file = None
+seqs = 0
+cluster = 0
+passed_first_chunk = False
+filename = None
+cluster_name = None
 prev_line = None
+arr = []
 
 # read and process input file
 with args.input as infile:
     for line in infile:
-        # check if adjacent lines are the same
+        # check if two consecutive lines are identical
         if line == prev_line:
-            # if lines are the same, close file and create next file
-            if current_file is not None:
-                current_file.close()
-            file_count += 1
-            output_filename = f"{file_count:03d}.fasta"
-            current_file = open(output_filename, "w")
+            filename = (
+                f"{seqs - 1 if seqs > 1 else 1}-seqs-{cluster_name}-cluster.fasta"
+            )
+            # write all lines minus the previous line to new file
+            if passed_first_chunk is True:
+                with open(filename, "w") as outfile:
+                    for a in arr[:-1]:
+                        outfile.write(a)
+            # reset array, make new name for next filename
+            arr = []
+            cluster_name = re.sub(r"[./]", "-", line.rstrip())
+            cluster_name = re.sub(r"[>]", "", cluster_name)
+            seqs = 0
+            passed_first_chunk = True
 
-        # write current line to current file
-        if current_file is not None:
-            current_file.write(line)
+        # collect line into an array
+        arr.append(line)
+
+        # count number of fasta headers in cluster chunk
+        if line.startswith(">"):
+            seqs += 1
 
         # iterate to next line
         prev_line = line
+
+    # write last fastas to final file
+    if filename is not None:
+        filename = f"{seqs - 1 if seqs > 1 else 1}-seqs-{cluster_name}-cluster.fasta"
+        with open(filename, "w") as outfile:
+            for a in arr[:-1]:
+                outfile.write(a)
 
 # my notes:
 # no need to wrap "args.input" in the "with open(args.input, "r") as infile" function...
