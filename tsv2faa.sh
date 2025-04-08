039546f3fedfa7d4caf980deb8ca4e202c6a973a
jlingford <james.lingford@gmail.com>
2025-04-08 17:25:21 +1000

    add tsv2faa script

--- tsv2faa.sh
+++ tsv2faa.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
 
-for file in *.tsv; do
-    # without taxids in output results
-    awk -F'\t' 'BEGIN {OFS="\t"} {print $2,$NF}' ${file} |
-        sed -n '2,$p' |
-        sort -k1 |
-        uniq |
-        awk -F'\t' '{printf ">%s\n%s\n", $1,$2}' >${file%.*}.faa
-    echo "done ${file%.*}"
-done
+INPUT=$1
+
+awk '{ printf(">%s\n%s\n"), $1,$2 }' "${INPUT}"
