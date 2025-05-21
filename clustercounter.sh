7815e84f072e02b6a0828f57bf63d78606511d38
jlingford <james.lingford@gmail.com>
2025-05-21 10:28:31 +1000

    add new clustercounter script

---
+++ clustercounter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+fd seqs.faa | awk -F"/" '{print $NF}' | sort | awk -F"-" -vOFS="\t" '{print $1, $(NF-6), $(NF-5), $(NF-3), $(NF-2), $(NF-4),  $NF}' | sed 's/id//' | sed 's/_seqs.faa//' | sed '1i #name\tdetails\tcoverage\tcov-mode\tcluster-mode\tseqid\tno_rep_seqs'
