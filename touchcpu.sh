dd8a0a30894ddfcb832ea0d82952df33cf42c418
jlingford <james.lingford@gmail.com>
2025-06-17 21:50:10 +1000

    add touchcpu.sh

---
+++ touchcpu.sh
@@ -0,0 +1,21 @@
+touch slurm_cpu.sh
+chmod u+x slurm_cpu.sh
+
+echo "#!/bin/bash -l
+#SBATCH -D ./
+#SBATCH -J james
+#SBATCH --mem=367000
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --account=rp24
+#SBATCH --partition=genomics
+#SBATCH --qos=genomics
+#SBATCH --time=4:00:00
+#SBATCH --mail-user=james.lingford@monash.edu
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_OUT
+#SBATCH --error=log-%j.err
+#SBATCH --output=log-%j.out
+
+# ---
+" >>slurm_cpu.sh
