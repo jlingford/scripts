912f709b7597d1338b4e0b7c70e28e06217efa67
jlingford <james.lingford@gmail.com>
2025-08-18 15:14:25 +1000

    add new splitfaa_boltz script for boltz1

---
+++ splitfaa_boltz.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Check if the input file is provided
+if [ -z "$1" ]; then
+    echo "Error: must provide one argument"
+    echo "Usage: $(basename $0) [INPUT.fasta]"
+    exit 1
+fi
+
+# take input fasta
+input_file="$1"
+
+# create output_dir
+output_dir=${input_file##*/}
+output_dir=${output_dir%.*}
+mkdir -p "${output_dir}"
+
+# awk funciton to split fastas
+awk -v outdir="${output_dir}" '
+  BEGIN {
+    prev_seqname = ""
+  }
+  /^>/{
+    split($1, arr, ">")
+    seqname = arr[2]
+    gsub(/[\.\|]/, "_", seqname)
+    gsub(/[#%^\*\\+!={}?]/, "", seqname)
+    if (seqname != prev_seqname && outfile) {
+      close(outfile)
+    }
+    outfile = outdir "/" seqname ".fasta"
+    prev_seqname = seqname
+  }
+  {
+    print $0 >> outfile
+  }
+' "${input_file}"
+
+# rename the fasta headers for chai input
+for file in "${output_dir}"/*.fasta; do
+    name=${file##*/}
+    name=${name%%.*}
+
+    sed -i "s#^>.*#>A|protein|./msas/${output_dir}/${name}.a3m#" "${file}"
+
+done
+
+# mv *.faa ${output_dir}
+# mv ${output_dir}/${input_file} .
